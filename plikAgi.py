e20a88cdd81b9ca2ddd829f898a575e66de9fc67
Agnieszka Gontarek <agazgon@gmail.com>
2017-03-22 13:53:53 +0100

    filtrowanie wersja próbna

--- plikAgi.py
+++ plikAgi.py
@@ -3,6 +3,7 @@ import numpy as np
 from utils import bresenhams_line
 from matplotlib import pyplot as plt
 import skimage.morphology as mp
+from skimage import img_as_float, img_as_ubyte
 
 def sinogram2picture(picture, sinogram, lines):
     #TODO transformations
@@ -17,12 +18,20 @@ def sinogram2picture(picture, sinogram, lines):
     reconstructed = np.zeros(shape = picture_shape)
     helper = np.zeros(shape = picture_shape)
     
+    print("Chce pofiltrować")
+    filtered_sinogram = filtering_sinogram(sinogram)
+    fig, plots = plt.subplots(1, 2)
+    plots[0].imshow(picture, cmap='gray')
+    plots[1].imshow(filtered_sinogram, cmap='gray')
+    plt.show()
+    print("Powinnam już skończyć filtrować")
+    
     # k-ta projekcja
     for projection in range (0, number_of_projections, 1):
         for detector in range (0, number_of_detectors, 1):
             x0, y0, x1, y1 = lines[projection][detector]
             line = bresenhams_line(x0, y0, x1, y1)
-            value = sinogram[projection][detector]
+            value = filtered_sinogram[projection][detector]
             for i in range (0, len(line), 1):
                     x, y = line[i]
                     if x >= 0 and y >= 0 and x < width and y < height:
@@ -40,11 +49,53 @@ def sinogram2picture(picture, sinogram, lines):
 
     print("lol")
     
-    
-    return normalizing(reconstructed, helper)
+    normalized = normalizing(reconstructed, helper)
+    #filtered = filtering_picture(normalized)
+    return normalized
 
-#def filtering(picture):
-#    return mp.erosion(picture)
+def filtering_sinogram(sinogram):
+    
+    sinogram_shape = np.shape(sinogram)
+    number_of_projections = sinogram_shape[0]
+    number_of_detectors = sinogram_shape[1]
+    
+    filtered = np.zeros((number_of_projections, number_of_detectors))
+    
+    mask = np.zeros((number_of_projections, number_of_detectors))
+    
+    for projection in range (0, number_of_projections, 1):
+        for detector in range (0, number_of_detectors, 1):
+            if (detector == 0):
+                mask[projection][detector] = 1
+            if (detector % 2 == 0):
+                mask[projection][detector] = 0
+            else:
+                mask[projection][detector] = (-4/(np.pi*np.pi))/(detector*detector)
+     
+    for projection in range (0, number_of_projections, 1):
+        for detector in range (0, number_of_detectors, 1):
+            if(detector != 0 and detector != number_of_detectors-1):
+                for m in range (-1, 2, 1):
+                    if(m == -1 or m == 1):
+                        filtered[projection][detector] += sinogram[projection][detector+m]*(-4/(np.pi*np.pi))
+                    if(m == 0):
+                        filtered[projection][detector] += sinogram[projection][detector+m]
+            else:
+                filtered[projection][detector] += sinogram[projection][detector]
+            #print ("f[", projection, "][", detector, "]: " , filtered[projection][detector])
+    print("SINGORAM:")
+    #print(sinogram[0:5][0:5])
+    print("FILTERED SINOGRAM:")
+    #print(filtered[0:5][0:5])
+    return filtered
+
+def filtering_picture(img) :
+    img = img_as_ubyte(img)
+    mean = np.mean(img)
+    binary = (img > mean*1.05) * 255
+    binary = np.uint8(binary)
+    return binary
+                
 
 def normalizing(reconstructed, helper):
     normalized = np.copy(reconstructed)
